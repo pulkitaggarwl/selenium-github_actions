9dfe474c0fe13acdee6fe6305bd7db9ba995aef3
pulkitaggarwl <36596168+pulkitaggarwl@users.noreply.github.com>
2021-08-08 11:43:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -64,6 +64,36 @@ class LoginForm(unittest.TestCase):
                 data={'description':"AssertionError: " + str(e)})
             self.test_result = 'fail'
             raise
+            
+            
+       def test_CBT1(self):
+    
+        try:
+            self.driver.get('http://crossbrowsertesting.github.io/login-form.html')
+            self.driver.maximize_window()
+            self.driver.find_element_by_name('username').send_keys('tester@crossbrowsertesting.com')
+            self.driver.find_element_by_name('password').send_keys('test123')
+            self.driver.find_element_by_css_selector('body > div > div > div > div > form > div.form-actions > button').click()
+
+            elem = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id=\"logged-in-message\"]/h2'))
+            )
+
+            welcomeText = elem.text
+            self.assertEqual("Welcome tester@crossbrowsertesting.com", welcomeText)
+
+            print("Taking snapshot")
+            snapshot_hash = self.api_session.post('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id + '/snapshots').json()['hash']
+
+            self.test_result = 'pass'
+
+        except AssertionError as e:
+            # log the error message, and set the score to "during tearDown()".
+            self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id + '/snapshots/' + snapshot_hash,
+                data={'description':"AssertionError: " + str(e)})
+            self.test_result = 'fail'
+            raise
+
 
     def tearDown(self):
         print("Done with session %s" % self.driver.session_id)
